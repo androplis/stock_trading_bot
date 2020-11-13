72903920efa8fccb1b7e942a5f5a1ee1671c0cd5
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-11-13 17:26:46 -0500

    Switched from Alpha Vantage to yfinance

--- stocks.py
+++ stocks.py
@@ -1,7 +1,6 @@
-import requests
+import yfinance as yf
 
-url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=WOKV6XE67340LADZ"
-r = requests.get(url)
+aapl = yf.Ticker("aapl")
+aaplHistory = aapl.history(period="1d", interval="1m")
 
-result_dict = r.json()
-items = result_dict['Time Series (5min)']
+print(aaplHistory['Close'])
