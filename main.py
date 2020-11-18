ef97ff67646b7e8f04d3512fad4589e407c9cb4d
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-11-17 21:50:19 -0500

    Implemented trading strategy 1.0

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import yfinance as yf
 import time
+from datetime import datetime
 from stocks import getStock
 
-buy = True
-balance = 1000.00
-
-def getStockPrice(stock):
+def getMarketPrice(stock):
     """ Returns the last closing price of the given stock"""
     day_history =  stock.history(period="1d", interval="1m")
-    return day_history["Close"][0]
-
+    return day_history["Close"][-1]
 
 def buySell():
-    stock = getStock("aapl")
-    last_close = getStockPrice(stock)
-    
-    
+    """ Performs a buy/sell strategy"""
     if buy:
         # Implement buy strategy
-        if balance > 0: # Check if money needs to be invested
+        if balance > 0: # Check if there is money to be invested
+            if market_price <= stock.info["dayLow"] + .50: # Buy if price reaches (or nears) daily low 
+                # Buy
+                print("Buy")
+            elif market_price >= stock.info["dayHigh"] - .50: # Buy if price reaches (or nears) daily high
+                # Buy
+                print("Buy")
+            else:
+                print("Nothing", stock.info["dayLow"])
     else:
-        print("idk")
-        # Implement sell strategy
+        if(market_price >= PROFIT_THRESHHOLD or market_price <= STOP_LOSS_THRESHHOLD):
+            # Sell
+            print("Sell")
+        else:
+            print("Hold")
 
+if __name__ == "__main__":
+    buy = True
+    balance = 1000.00
+    stock = getStock("aapl")
 
-# Perform Trading
-while True:
-    buySell()
-    time.sleep(10) # Run every 10 seconds
+    market_price = getMarketPrice(stock)
+    PROFIT_THRESHHOLD = 0
+    STOP_LOSS_THRESHHOLD = 0
+
+    # Perform Trading
+    while True:
+        buySell()
+        time.sleep(60) # Run every 60 seconds
--- stocks.py
+++ stocks.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+from datetime import datetime
 
 def getStock(ticker):
     stock = yf.Ticker(ticker)
