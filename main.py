36b0734e35ca25f45eaaeaf47fd3ea5dd51539b2
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-12-12 20:54:38 -0500

    Added stock class

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import yfinance as yf
 import time
 from datetime import datetime
+import stocks
+
+class Portfolio:
+    """ Investor's portfolio, contains a balance and a set of watch stocks """
+    def __int__(self, balance):
+        self.balance = balance
+        self.watch_stocks = list()
+
+    def getBalance(self):
+        return self.balance
+
+    def setBalance(self, amount):
+        self.balance = amount
+
+    def buySell(self):
+        """ Looks at each stock and determines whether to buy or sell """
+        # For stock in stocks
+        # Implement Stratey 
 
 if __name__ == "__main__":
 
--- stocks.py
+++ stocks.py
@@ -2,9 +2,12 @@ import yfinance as yf
 from datetime import datetime
 
 class Stock:
-    def __init__(self):
-        self.ticker = ''
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.shares = 0
+        self.buy = True
 
+    # Accessors
     def getStock(self):
         """ Returns a stock object """
         return yf.Ticker(self.ticker)
@@ -14,3 +17,27 @@ class Stock:
         dayHistory = self.getStock().history(period="1d", interval="1m")
         return dayHistory["Close"][-1]
 
+    def getMarketValue(self):
+        """ Returns the market value of the stock shares owned"""
+        return self.getClose() * self.shares
+
+    # Buy / Sell
+    def buyStock(self, numShares):
+        """ Buys (numShares) amount of stock at the last closing price """
+        self.shares = numShares
+        self.buy = False
+
+    def sellStock(self, numShares):
+        """ Sells (numShares) amount of stock at the last closing price """
+        self.shares = numShares
+        self.buy = True
+
+def getStock(tickers):
+    # Select which stocks with web-scraping...
+
+    stocks = list()
+    for ticker in tickers:
+        stock = Stock(ticker)
+        stocks.append(stock)
+
+    return stocks
