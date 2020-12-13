f27b76291c439d13d13c6d2042b4def43adde987
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-12-12 20:00:08 -0500

    Added stock class, removed main functions

--- main.py
+++ main.py
@@ -1,43 +1,10 @@
 import yfinance as yf
 import time
 from datetime import datetime
-from stocks import getStock
-
-def getMarketPrice(stock):
-    """ Returns the last closing price of the given stock"""
-    day_history =  stock.history(period="1d", interval="1m")
-    return day_history["Close"][-1]
-
-def buySell():
-    """ Performs a buy/sell strategy"""
-    if buy:
-        # Implement buy strategy
-        if balance > 0: # Check if there is money to be invested
-            if market_price <= stock.info["dayLow"] + .50: # Buy if price reaches (or nears) daily low 
-                # Buy
-                print("Buy")
-            elif market_price >= stock.info["dayHigh"] - .50: # Buy if price reaches (or nears) daily high
-                # Buy
-                print("Buy")
-            else:
-                print("Nothing", stock.info["dayLow"])
-    else:
-        if(market_price >= PROFIT_THRESHHOLD or market_price <= STOP_LOSS_THRESHHOLD):
-            # Sell
-            print("Sell")
-        else:
-            print("Hold")
 
 if __name__ == "__main__":
-    buy = True
-    balance = 1000.00
-    stock = getStock("aapl")
-
-    market_price = getMarketPrice(stock)
-    PROFIT_THRESHHOLD = 0
-    STOP_LOSS_THRESHHOLD = 0
 
     # Perform Trading
     while True:
-        buySell()
+        # Buy / # Sell
         time.sleep(60) # Run every 60 seconds
--- stocks.py
+++ stocks.py
@@ -1,7 +1,16 @@
 import yfinance as yf
 from datetime import datetime
 
-def getStock(ticker):
-    stock = yf.Ticker(ticker)
-    return stock
+class Stock:
+    def __init__(self):
+        self.ticker = ''
+
+    def getStock(self):
+        """ Returns a stock object """
+        return yf.Ticker(self.ticker)
+
+    def getClose(self):
+        """ Returns the last closing price of the stock """
+        dayHistory = self.getStock().history(period="1d", interval="1m")
+        return dayHistory["Close"][-1]
 
